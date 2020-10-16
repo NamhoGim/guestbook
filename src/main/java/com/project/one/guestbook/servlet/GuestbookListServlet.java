ec6e92e5bf9ad60a1489ea050ff72e8e6bf167d9
limjoonchul <dlawnscjf511@naver.com>
2020-10-16 13:02:03 +0900

    feat: doGet update
    doGet update

--- src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
+++ src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
@@ -3,6 +3,7 @@ package com.project.one.guestbook.servlet;
 import com.project.one.guestbook.dao.GuestbookDao;
 import com.project.one.guestbook.dto.Guestbook;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +17,10 @@ public class GuestbookListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         GuestbookDao dao = new GuestbookDao();
-        List<Guestbook> list =  dao.getGuestBooks();
+        List<Guestbook> guestbookList =  dao.getGuestBooks();
+        req.setAttribute("guestbooks",guestbookList);
 
-        resp.getWriter();
+        RequestDispatcher rd = req.getRequestDispatcher("/guestbooks.jsp");
+        rd.forward(req,resp);
     }
 }
--- src/main/java/com/project/one/guestbook/servlet/GuestbookWriteServlet.java
+++ src/main/java/com/project/one/guestbook/servlet/GuestbookWriteServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/guestbooks/write")
 public class GuestbookWriteServlet extends HttpServlet {

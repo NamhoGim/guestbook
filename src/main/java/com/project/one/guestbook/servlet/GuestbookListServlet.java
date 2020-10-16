bfba07a65b8a2fa9816f2ab2f24c9f522f182d39
limjoonchul <dlawnscjf511@naver.com>
2020-10-16 11:10:45 +0900

    checkpoint

--- src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
+++ src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
@@ -1,4 +1,19 @@
 package com.project.one.guestbook.servlet;
 
-public class GuestbookListServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/guestbooks")
+public class GuestbookListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String content = req.getParameter("content");
+
+
+    }
 }

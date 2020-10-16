6c65f17264a966934ae334bb4ce1fb97754b4824
limjoonchul <dlawnscjf511@naver.com>
2020-10-16 17:45:06 +0900

    feat: dopost, addguestbook method implemented
    dopost, addguestbook method implemented

--- src/main/java/com/project/one/guestbook/dao/GuestbookDao.java
+++ src/main/java/com/project/one/guestbook/dao/GuestbookDao.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 import com.project.one.guestbook.dto.Guestbook;
@@ -35,5 +35,19 @@ public class GuestbookDao {
         return guestbooks;
     }
 
-    public void addGuestBook(Guestbook guestbook) {}
+    public void addGuestBook(Guestbook guestbook) {
+        String sql = "INSERT INTO user(user_name, description, datetime) VALUES(?, ?, NOW())";
+
+        try(Connection conn = DBUtil.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, guestbook.getName());
+            stmt.setString(2, guestbook.getContent());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/project/one/guestbook/servlet/GuestbookWriteServlet.java
+++ src/main/java/com/project/one/guestbook/servlet/GuestbookWriteServlet.java
@@ -1,17 +1,33 @@
 package com.project.one.guestbook.servlet;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import com.project.one.guestbook.dao.GuestbookDao;
+import com.project.one.guestbook.dto.Guestbook;
 
 @WebServlet("/guestbooks/write")
 public class GuestbookWriteServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       doGet(req,resp);
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text.html; charset=UTF-8");
+
+        String name = req.getParameter("name");
+        String description = req.getParameter("content");
+
+        Guestbook guestbook = new Guestbook(name, description);
+        GuestbookDao guestbookDao = new GuestbookDao();
+
+        guestbookDao.addGuestBook(guestbook);
+
+
+        resp.sendRedirect("/guestbooks");
     }
 }

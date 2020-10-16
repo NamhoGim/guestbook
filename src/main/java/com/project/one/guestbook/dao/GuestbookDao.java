976b190d3e29b2fdc6898b3bf6b16fa38b472fcc
limjoonchul <dlawnscjf511@naver.com>
2020-10-16 14:22:03 +0900

    feat: get data (#5)
    * feat: get DB
    
    get data
    
    * rename
    
    datetime rename

--- src/main/java/com/project/one/guestbook/dao/GuestbookDao.java
+++ src/main/java/com/project/one/guestbook/dao/GuestbookDao.java
@@ -1,13 +1,38 @@
 package com.project.one.guestbook.dao;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.ArrayList;
+
 
 import com.project.one.guestbook.dto.Guestbook;
+import com.project.one.guestbook.util.DBUtil;
 
 public class GuestbookDao {
     public List<Guestbook> getGuestBooks() {
-        return new ArrayList<>();
+
+        List<Guestbook> guestbooks = new ArrayList<>();
+        String sql = "SELECT * FROM user";
+
+        try(Connection conn = DBUtil.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()){
+                Guestbook guestbook = new Guestbook (rs.getLong("id"), rs.getString("user_name")
+                        , rs.getString("description"), rs.getDate("datetime"));
+
+                guestbooks.add(guestbook);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return guestbooks;
     }
 
     public void addGuestBook(Guestbook guestbook) {}

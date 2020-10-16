3154dc9b201a4b6e8b78f13727ccd86c9fe944e9
limjoonchul <dlawnscjf511@naver.com>
2020-10-16 11:42:14 +0900

    checkpoint

--- src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
+++ src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
@@ -1,19 +1,23 @@
 package com.project.one.guestbook.servlet;
 
+import com.project.one.guestbook.dao.GuestbookDao;
+import com.project.one.guestbook.dto.Guestbook;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/guestbooks")
 public class GuestbookListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String content = req.getParameter("content");
+        GuestbookDao dao = new GuestbookDao();
+        List<Guestbook> list =  dao.getGuestBooks();
 
-
+        resp.getWriter();
     }
 }

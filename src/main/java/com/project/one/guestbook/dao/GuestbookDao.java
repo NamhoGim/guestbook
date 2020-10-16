1a8519b6b9c577e06b0439012838d03e3a2f6995
NamhoGim <kkjknh2@gmail.com>
2020-10-16 11:01:09 +0900

    add: dummy GuestbookDao

--- src/main/java/com/project/one/guestbook/dao/GuestbookDao.java
+++ src/main/java/com/project/one/guestbook/dao/GuestbookDao.java
@@ -1,4 +1,14 @@
 package com.project.one.guestbook.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.project.one.guestbook.dto.Guestbook;
+
 public class GuestbookDao {
+    public List<Guestbook> getGuestBooks() {
+        return new ArrayList<>();
+    }
+
+    public void addGuestBook(Guestbook guestbook) { }
 }

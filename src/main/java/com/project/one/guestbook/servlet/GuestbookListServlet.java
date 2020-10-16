746a0ad6d5524f22fe0c151aec95f2a8ce766876
limjoonchul <dlawnscjf511@naver.com>
2020-10-16 13:12:07 +0900

    Serveltget (#4)
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * feat: doGet update
    
    doGet update
    
    * Revise package ordering
    
    Revise package ordering

--- src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
+++ src/main/java/com/project/one/guestbook/servlet/GuestbookListServlet.java
@@ -1,4 +1,30 @@
 package com.project.one.guestbook.servlet;
 
-public class GuestbookListServlet {
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.project.one.guestbook.dao.GuestbookDao;
+import com.project.one.guestbook.dto.Guestbook;
+
+
+@WebServlet("/guestbooks")
+public class GuestbookListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        GuestbookDao dao = new GuestbookDao();
+        List<Guestbook> guestbookList =  dao.getGuestBooks();
+        req.setAttribute("guestbooks",guestbookList);
+
+        RequestDispatcher rd = req.getRequestDispatcher("/guestbooks.jsp");
+        rd.forward(req,resp);
+    }
 }
--- src/main/java/com/project/one/guestbook/servlet/GuestbookWriteServlet.java
+++ src/main/java/com/project/one/guestbook/servlet/GuestbookWriteServlet.java
@@ -1,4 +1,17 @@
 package com.project.one.guestbook.servlet;
 
-public class GuestbookWriteServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/guestbooks/write")
+public class GuestbookWriteServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       doGet(req,resp);
+    }
 }

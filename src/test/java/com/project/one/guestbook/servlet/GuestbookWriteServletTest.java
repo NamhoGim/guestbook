d144e132ad700a6e87074ae17392a9daf6403375
NamhoGim <kkjknh2@gmail.com>
2020-10-20 23:02:59 +0900

    add unit test for doPost method of GuestbookWriteServlet

---
+++ src/test/java/com/project/one/guestbook/servlet/GuestbookWriteServletTest.java
@@ -0,0 +1,53 @@
+package com.project.one.guestbook.servlet;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.project.one.guestbook.dao.GuestbookDao;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GuestbookWriteServletTest {
+
+    @Mock private HttpServletRequest request;
+    @Mock private HttpServletResponse response;
+    @Mock private RequestDispatcher dispatcher;
+    @Mock private ServletContext context;
+
+    @Mock private GuestbookDao guestbookDao;
+
+    @Before
+    public void setUp() {
+        assertNotNull(request);
+        assertNotNull(response);
+        assertNotNull(dispatcher);
+        assertNotNull(context);
+
+        assertNotNull(guestbookDao);
+
+        when(request.getParameter("name")).thenReturn("test-name");
+        when(request.getParameter("content")).thenReturn("test-content");
+    }
+
+    @Test
+    public void guestbookWriteServlet() throws ServletException, IOException {
+        GuestbookWriteServlet guestbookWriteServlet = new GuestbookWriteServlet(guestbookDao);
+
+        guestbookWriteServlet.doPost(request, response);
+
+        verify(response).sendRedirect("/guestbooks");
+    }
+}
